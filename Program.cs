5820f7049283cf6cab64f8b236b99bbf0386c10e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-05 22:28:34 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿    int numero, multiplo, resultado;
+
+    Console.WriteLine("Digite um número:............");
+    numero = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Avaliar se é múltiplo:.......");
+    multiplo = int.Parse(Console.ReadLine()!);
+    
+    resultado = numero / multiplo;
+
+    if (numero % multiplo == 0) 
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{numero} é multiplo por {multiplo}");
+    }
+    else 
+    {
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{numero} não é multiplo por {multiplo}");
+    }
